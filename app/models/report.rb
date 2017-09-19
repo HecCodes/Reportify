4b02d883ff31ec096e97b6e1f31da43200cf2c0d
Codelikeyouwantit <HectorFeurtado@gmail.com>
2017-09-19 11:29:27 -0400

    add reports to seed

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,9 @@
 class Report < ApplicationRecord
-  validates :test_quizes,:homework,:classwork,:punctuality_attendance,:contributions,:collab_coop, :number, :inclusion => 0..11, numericality:true, presence: true
-  validates :openness_to_feedback,:takes_responsibility,:number, :inclusion => 0..10,numericality:true, presence: true
-  validates :consistently_good_effort,:number, :inclusion => 0..14,numericality:true, presence: true
+  validates :test_quizes,:homework,:classwork,:punctuality_attendance,:contributions,:collab_coop, numericality:{only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 11}, presence: true
+  validates :openness_to_feedback,:takes_responsibility, numericality:{only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}, presence: true
+  validates :consistently_good_effort, numericality:{only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 14}, presence: true
   validates :teacher_id, :student_id, presence: true
-  validates :date_range, presence:true
+  validates :date, presence:true
 
   belongs_to :teacher, class_name: :User
   belongs_to :student
