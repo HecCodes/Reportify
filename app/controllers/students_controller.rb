d1e65b0e76232c9bcbacd21686542718630aeb25
Codelikeyouwantit <HectorFeurtado@gmail.com>
2017-09-19 14:14:29 -0400

    add student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,11 +7,27 @@ class StudentsController < ApplicationController
   end
 
   def new
-
+    @parents = Parent.all
+    @student = Student.new
   end
 
   def create
+    space = params[:parent].index(" ")
+    first_name = params[:parent][0..space-1]
+    last_name = params[:parent][space+1..-1]
 
+    @student = Student.new(
+      first_name:params[:student][:first_name],
+      last_name:params[:student][:last_name],
+      teacher_id: current_user.id,
+      parent_id: Parent.find_by(first_name:first_name, last_name:last_name).id
+    )
+    if @student.save
+      redirect_to students_path
+    else
+      @errors = @student.errors
+      render 'new'
+    end
   end
 
   def show
@@ -29,5 +45,4 @@ class StudentsController < ApplicationController
   def destroy
 
   end
-
 end
